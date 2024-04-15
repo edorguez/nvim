5209bc03c0f2331290b8944f4912b1acc9d65ca5
Eduardo Rodríguez <edro998@gmail.com>
2024-04-14 20:39:42 -0400

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>ai", ":lua vim.lsp.buf.code_action() <cr>", { desc = ""Import component/file under cursor }
+map("n", "<leader>ai", ":lua vim.lsp.buf.code_action() <cr>", { desc = "Import component/file under cursor" }
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
